ea78b46e53d70fd83bd652cb97e67e8f92b75326
David <david.omeara91@gmail.com>
2022-08-18 23:34:46 +0100

    mark done

--- src/components/TaskCard/TaskCard.component.jsx
+++ src/components/TaskCard/TaskCard.component.jsx
@@ -1,14 +1,8 @@
 import React from "react";
-import { useTasks } from "../../hooks/useTasks";
-import { Card } from "@mui/material";
-import { CardContent } from "@mui/material";
-import { Typography } from "@mui/material";
+
 import { Button } from "@mui/material";
-import { CircularProgress } from "@mui/material";
 import './TaskCard.styles.css'
 import TaskOutlinedIcon from "@mui/icons-material/Task";
-import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import moment from "moment";
 
 export const TaskCard = (props)=>{
@@ -16,7 +10,7 @@ export const TaskCard = (props)=>{
     const CardStyle = {
         backgroundColor: props.done ? "inherit": "white",
         boxShadow : props.done ? "none" : "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px",
-        border: props.done ? ".5px solid lightgrey" : "none"
+        border: props.done ? ".5px solid lightgrey" : "none",
     }
 
     const iconStyle = {
@@ -34,7 +28,7 @@ export const TaskCard = (props)=>{
            
 
             {props.done ? null : 
-            <Button id="card-button" size="small">Done</Button>
+            <Button onClick={()=>props.handleMarkDone(props.id)} id="card-button" size="small">Done</Button>
             }
 
 
--- src/components/TaskList/TaskList.component.js
+++ src/components/TaskList/TaskList.component.js
@@ -9,16 +9,31 @@ import { TaskCard } from "../TaskCard/TaskCard.component";
 
 export const TaskList = ()=>{
 
-    const {tasks, loadingStatus } = useTasks();
+    const {tasks, loadingStatus, setTasks } = useTasks();
 
-    const outstandingTasks = tasks.filter(task=>task.completed==false)
+    const outstandingTasks = tasks.filter(task=>task.completed==false);
+
+    const handleMarkDone = (taskId)=>{
+        let updatedTasks = [...tasks];
+        let index = updatedTasks.findIndex(task=>task.id==taskId);
+        updatedTasks[index].completed=true;
+        setTasks(updatedTasks);
+
+        // 2. PUT request
+
+
+        console.log(taskId);
+    }
+
+
+
     return(
         <>
          { loadingStatus==="loading" ?  <CircularProgress/> : 
          <>
          <p>{outstandingTasks.length} outstanding tasks</p>
             {tasks.map(task=>(
-                <TaskCard title={task.title} dueDate={task.due} done={task.completed}/>
+                <TaskCard title={task.title} dueDate={task.due} done={task.completed} handleMarkDone={handleMarkDone} id={task.id} key={task.id}/>
             ))}
             </>
         }
