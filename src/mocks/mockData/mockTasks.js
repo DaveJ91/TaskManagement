30f5862ef86b83eec0694bb98fe27acba835af2e
David <david.omeara91@gmail.com>
2022-08-20 10:05:08 +0100

    feat: add tests

---
+++ src/mocks/mockData/mockTasks.js
@@ -0,0 +1,47 @@
+export const mockTasks = [
+    {
+     "createdAt": "2022-08-17T03:19:33.924Z",
+     "title": "File Annual B1 return",
+     "assignee": "assignee 1",
+     "due": "2023-02-10T11:47:13.489Z",
+     "completed": false,
+     "category": "not important",
+     "id": "1"
+    },
+    {
+     "createdAt": "2022-08-17T11:18:36.341Z",
+     "title": "Sign Audited Financial Statements",
+     "assignee": "assignee 2",
+     "due": "2022-12-06T05:56:15.732Z",
+     "completed": false,
+     "category": "category 2",
+     "id": "2"
+    },
+    {
+     "createdAt": "2022-08-17T01:45:28.584Z",
+     "title": "Prepare Shareholders Letter",
+     "assignee": "assignee 3",
+     "due": "2023-07-27T23:33:36.850Z",
+     "completed": true,
+     "category": "category 3",
+     "id": "3"
+    },
+    {
+     "createdAt": "2022-08-17T11:59:51.459Z",
+     "title": "Review Draft Quarterly Report",
+     "assignee": "assignee 4",
+     "due": "2023-04-13T15:47:59.724Z",
+     "completed": true,
+     "category": "category 4",
+     "id": "4"
+    },
+    {
+     "createdAt": "2022-08-17T16:50:52.144Z",
+     "title": "AGM prep",
+     "assignee": "assignee 5",
+     "due": "2022-08-30T02:41:54.115Z",
+     "completed": true,
+     "category": "category 5",
+     "id": "5"
+    }
+   ]
---
+++ src/reducers/tasksReducer.spec.js
@@ -0,0 +1,31 @@
+import { tasksReducer }from './tasksReducer.js';
+import { mockTasks } from '../mocks/mockData/mockTasks'
+import { cleanup } from '@testing-library/react';
+
+describe("tests for tasks reducer", ()=>{
+
+    afterEach(()=>{
+        cleanup()
+    })
+    test ("default returns all tasks", ()=>{
+        const testInput = mockTasks;
+        const testAction = {
+            type: "",
+        };
+        const reducerOutput = tasksReducer(testInput, testAction);
+        expect(reducerOutput).toStrictEqual(testInput)
+    })
+
+    test("mark done will set a tasks completed property to true", ()=>{
+        const testInput = mockTasks;
+        testInput[0].completed=false;
+        const testIncompleteTask =testInput[0];
+        expect(testIncompleteTask.completed).toBeFalsy();
+        const testAction = {
+            type: "MARK_DONE",
+            taskId: testIncompleteTask.id
+        };
+        const reducerOutput = tasksReducer(testInput, testAction);
+        expect(reducerOutput[0].completed).toBeTruthy();
+    })
+})
