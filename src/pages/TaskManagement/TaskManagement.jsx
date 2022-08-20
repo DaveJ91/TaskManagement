84a0bfeaefde939714b6c128033bf6b81e1803a0
David <david.omeara91@gmail.com>
2022-08-20 11:05:26 +0100

    feat: add filter

--- src/pages/TaskManagement/TaskManagement.jsx
+++ src/pages/TaskManagement/TaskManagement.jsx
@@ -8,11 +8,18 @@ import { useTasks } from '../../hooks/useTasks'
 import {v4 as uuidv4} from 'uuid';
 import Modal from '@mui/material/Modal';
 import AddTaskModal from '../../components/AddTaskModal/AddTaskModal.component';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
 export const TaskManagement = () => {
 
     const {tasks, dispatch, loadingStatus} = useTasks();
     const [modalOpen, setModalOpen] = useState(false);
+    const [filterValue, setFilterValue] = useState("all")
 
     const handleClick = ()=>{
         setModalOpen(true)
@@ -37,17 +44,48 @@ export const TaskManagement = () => {
     }
 
     const outstandingTasks = tasks.filter(t=>t.completed===false)
+    const completedTasks = tasks.filter(t=>t.completed===true)
 
-    console.log(tasks)
+    let filteredTasks; 
+
+    switch(filterValue){
+        case "all":
+            filteredTasks=tasks;
+            break;
+        case "completed":
+            filteredTasks=completedTasks;
+            break
+        case "outstanding":
+            filteredTasks=outstandingTasks;
+            break;
+    }
+    
 
     return (
         <div className="container">
             <Button onClick={handleClick}>Add Task</Button>
+
+            
+            <div>
+            <ToggleButtonGroup
+                color="primary"
+                value={filterValue}
+                exclusive
+                onChange={(e)=>setFilterValue(e.target.value)}
+                aria-label="Platform"
+            >
+            <ToggleButton value="all">All</ToggleButton>
+            <ToggleButton value="outstanding">Outstanding</ToggleButton>
+            <ToggleButton value="completed">Completed</ToggleButton>
+            </ToggleButtonGroup>
+            </div>
+            
+
             <h2>Task Manangement</h2>
             { loadingStatus==="loading" ?  <CircularProgress/> : 
                 <div>
                     <p>{outstandingTasks.length} outstanding tasks</p>
-                        { tasks.map(task=>
+                        { filteredTasks.map(task=>
                             <TaskCard key={task.id} title={task.title} dueDate={task.due} done={task.completed} handleMarkDone={handleMarkDone} id={task.id} />
                         )
             }
